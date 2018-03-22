b12fbb9b5ed4d9b405b74f7a006d655784ab4895
glavie <glavie22@mail.ru>
2018-03-22 11:37:33 +0200

    Update images.py

--- images.py
+++ images.py
@@ -10,8 +10,13 @@ import urllib.request
 xls = pd.ExcelFile('/home/v.efimenko/test.Export_test.xlsx')
 df = xls.parse(header=0, index_col=None).fillna('')
 
+#Итерируемся по строкам DF
 for row in df.iterrows():
+	#Поскольку в поле ссылки на изображение может быть несколько изображений - необходимо дополнительно итерироваться по
+	#списку изображений для каждой позиции.
 	for link in row[1]['Ссылка_изображения'].split(', '):
-		print(link)
+		#На данном этапе происходит загрузка изображения с присваиванием порядкового номера в соответствии с файлом 
+		#экспорта и соответствующего формата картинки.
 		urllib.request.urlretrieve(link, '/home/v.efimenko/test/image {0}.{1}'.format(str(row[0] + 2), link.split('.')[4]))
+		#Дабы не напороться на капчу добавляем интервал выполнения итераций
 		time.sleep(3)
