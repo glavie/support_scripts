7ed41a555322a05289381a6d80eaede92b870f76
glavie <glavie22@mail.ru>
2018-04-06 16:42:01 +0300

    Update scraping_docs.py

--- scraping_docs.py
+++ scraping_docs.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 #На вход подается файл экспорта из GA по посещениям справки(первые 4 колонки),
-#с колонками: url, views, unique_views, time, video, img_count, text_doc, len_doc
+#с колонками: url, views, unique_views, time, img_count, video, text_doc, len_doc
 
 #Цель - заполнить остальные колонки и попытаться определить зависимость количества и времени просмотров от:
 #наличия видео на странице, количества изображений, длинны текста.
@@ -18,40 +18,33 @@ df = xls.parse('prom', header=0, index_col=None).fillna('')
 
 def scrapp(url):
 
-	#Для страниц документации
-	
+	url = 'http://support.prom.ua' + url
+	html = urlopen(url)
+	bsObj = BeautifulSoup(html)
+
 	if 'documents' in url:
 
-		url = 'http://support.prom.ua' + url
-		html = urlopen(url)
-		bsObj = BeautifulSoup(html)
 		doc_html = bsObj.findAll("div", {"class": "b-user-support__user-content b-user-support__user-content_type_document"})[0]
-		return [str(doc_html).count('alt=""'), '<iframe' in str(doc_html), doc_html.get_text().strip()]
 
 	elif 'questions' in url:
-	
-	#Для страниц с вопросами
-	
-		url = 'http://support.prom.ua' + url
-		html = urlopen(url)
-		bsObj = BeautifulSoup(html)
-		doc_html = bsObj.findAll("blockquote", {"class": "b-teachers-info h-layout-vm-20"})[0]
-		return [str(doc_html).count('alt=""'), '<iframe' in str(doc_html), doc_html.get_text().strip()]
 
-	else:
-	
-	#Для страниц навигации, которые попали в выборку (или поисковых запров)
-		return []
+		doc_html = bsObj.findAll("blockquote", {"class": "b-teachers-info h-layout-vm-20"})[0]
+		
+	return [str(doc_html).count('alt=""'), '<iframe' in str(doc_html), doc_html.get_text().strip()]
 
 
 for ind in range(len(df.index)):
+
+	url = df['url'][ind]
+
 	try:
-		print(df['url'][ind])
-		result = scrapp(df['url'][ind])
-		df.loc[ind] = [df['url'][ind], df['views'][ind], df['unique_views'][ind], df['time'][ind], result[1], result[0], result[2], len(result[2])]		
+		print(url)
+		result = scrapp(url)
+		df.loc[ind] = list(df.loc[ind][:4]) + result + [len(result[2])]		
 
 	except IndexError:
 		continue
+
 	except HTTPError:
 		continue
 
